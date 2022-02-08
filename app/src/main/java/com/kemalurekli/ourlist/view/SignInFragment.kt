83afe2e5dbe093b7f0585ee6352abfdc2fb4e4e6
kemalurekli <kemalurekli@gmail.com>
2022-02-08 16:04:52 +0300

    sign in up class created and view model viewbinding added

--- app/src/main/java/com/kemalurekli/ourlist/view/SignInFragment.kt
+++ app/src/main/java/com/kemalurekli/ourlist/view/SignInFragment.kt
@@ -7,25 +7,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import com.kemalurekli.ourlist.R
-import com.kemalurekli.ourlist.databinding.FragmentDetailsBinding
-import com.kemalurekli.ourlist.viewmodel.DetailsFragmentViewModel
+import com.kemalurekli.ourlist.databinding.FragmentSignInBinding
+import com.kemalurekli.ourlist.viewmodel.SignInFragmentViewModel
 
-class DetailsFragment : Fragment() {
-    private lateinit var binding: FragmentDetailsBinding
-    private lateinit var viewModel : DetailsFragmentViewModel
 
+class SignInFragment : Fragment() {
+    private lateinit var binding: FragmentSignInBinding
+    private lateinit var viewModel : SignInFragmentViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentDetailsBinding.inflate(layoutInflater)
-        viewModel = ViewModelProvider(this)[DetailsFragmentViewModel::class.java]
-        return inflater.inflate(R.layout.fragment_details, container, false)
+        binding = FragmentSignInBinding.inflate(layoutInflater)
+        viewModel = ViewModelProvider(this)[SignInFragmentViewModel::class.java]
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
 }
--- app/src/main/java/com/kemalurekli/ourlist/view/SignUpFragment.kt
+++ app/src/main/java/com/kemalurekli/ourlist/view/SignUpFragment.kt
@@ -7,25 +7,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import com.kemalurekli.ourlist.R
-import com.kemalurekli.ourlist.databinding.FragmentDetailsBinding
-import com.kemalurekli.ourlist.viewmodel.DetailsFragmentViewModel
+import com.kemalurekli.ourlist.databinding.FragmentSignUpBinding
+import com.kemalurekli.ourlist.viewmodel.SignUpFragmentViewModel
 
-class DetailsFragment : Fragment() {
-    private lateinit var binding: FragmentDetailsBinding
-    private lateinit var viewModel : DetailsFragmentViewModel
+class SignUpFragment : Fragment() {
+    private lateinit var binding: FragmentSignUpBinding
+    private lateinit var viewModel : SignUpFragmentViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentDetailsBinding.inflate(layoutInflater)
-        viewModel = ViewModelProvider(this)[DetailsFragmentViewModel::class.java]
-        return inflater.inflate(R.layout.fragment_details, container, false)
+        binding = FragmentSignUpBinding.inflate(layoutInflater)
+        viewModel = ViewModelProvider(this)[SignUpFragmentViewModel::class.java]
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
-
 }
